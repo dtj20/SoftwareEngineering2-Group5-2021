c506e8bf328215cb8cc3ae6d69c4d04ac6f2f034
Administrator <o.zorob@googlemail.com>
2021-03-14 20:59:43 +0000

    Removed issue with repeating nulls when an incorrect account has been inputted. Added password functionality for check login details.

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -1,13 +1,20 @@
 package newbank.server;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class Account {
 	
 	private String accountName;
 	private double openingBalance;
 
+
 	public Account(String accountName, double openingBalance) {
 		this.accountName = accountName;
 		this.openingBalance = openingBalance;
+
 	}
 	
 	public String toString() {
--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -5,18 +5,25 @@ import java.util.ArrayList;
 public class Customer {
 	
 	private ArrayList<Account> accounts;
-	
-	public Customer() {
+	private String password;
+
+	public Customer(String password) {
+		this.password = password;
 		accounts = new ArrayList<>();
 	}
-	
+
 	public String accountsToString() {
 		String s = "";
+
 		for(Account a : accounts) {
 			s += a.toString();
 		}
 		return s;
 	}
+
+	public ArrayList<Account> getAccounts(){return accounts;}
+	public String getPassword(){return password;}
+
 
 	public void addAccount(Account account) {
 		accounts.add(account);		

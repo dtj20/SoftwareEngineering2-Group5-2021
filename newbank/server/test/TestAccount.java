6972f2b1b42312034cd2fe5395b9a0fecedebda3
sean Temple <temple343@gmail.com>
2021-03-16 19:36:00 +0000

    Improved test data

--- newbank/server/test/TestAccount.java
+++ newbank/server/test/TestAccount.java
@@ -2,6 +2,8 @@ package newbank.server.test;
 
 import newbank.server.Account;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 
@@ -12,8 +14,19 @@ public class TestAccount {
         String Name = "Test Account";
         int Balance = 1500;
         Account acc = new Account(Name, Balance );
-        //assertEquals(Name, acc.getName());
-        //assertEquals(Balance, acc.getBalance());
-
+        assertEquals(Name, acc.getName());
+        assertTrue(Balance == acc.getBalance());
     }
+
+    @Test
+    public void testToString() {
+        String name = "name";
+        int balance = 1500;
+        String expected =  name + ": " + balance;
+        Account acc = new Account(name, balance);
+        assertEquals(expected, acc.toString());
+    }
+
+
+
 }
--- newbank/server/test/TestCustomer.java
+++ newbank/server/test/TestCustomer.java
@@ -9,18 +9,34 @@ public class TestCustomer {
 
     @Test
     public void testCustomerAccounts() {
-        Customer c = new Customer();
+        Customer c = new Customer("1234");
         String empty = "";
         assertEquals("Customer is not empty", empty, c.accountsToString());
     }
 
     @Test
     public void testCustomerAddAccount() {
-        Customer c = new Customer();
+        Customer c = new Customer("1234");
         String empty = "";
         Account acc = new Account("name", 1500);
         c.addAccount(acc);
         assertNotEquals("Customer is empty", empty, c.accountsToString());
+    }
+
+    @Test
+    public void testFindAccount() {
+        Customer c = new Customer("1234");
+        Account acc = new Account("name", 1500);
+        c.addAccount(acc);
+        Account test = c.findAccount("name");
+        assertEquals("accounts are not equal", acc, test);
+    }
+
+    @Test
+    public void testPasswordSet() {
+        String password = "1234";
+        Customer c = new Customer(password);
+        assertEquals("passwords do not match", c.getPassword(), password);
     }
 
 }

1b577585f5e58426ecd2a83187fe70cef3be2678
sean Temple <temple343@gmail.com>
2021-03-30 19:47:38 +0100

    Added the missing call option in menu option 1

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -61,8 +61,14 @@ public class NewBank {
 		if(customers.containsKey(customer.getKey())) {
 
 			if (request.equals("1")) {
-				return showMyAccounts(customer);
-			} else if (request.equals("2")) {
+				System.out.println(showMyAccounts(customer));
+				String response = menuResponseBuilder("Would you like to view the accounts transaction history? Y/N");
+				if(response.equals('Y')) {
+					String account = menuResponseBuilder("Please enter an account name");
+					showTransactionHistory(customer, account);
+				}
+				return "Returning to Main Menu";
+			}  else if (request.equals("2")) {
 				if(customers.get(customer.getKey())
 						.addAccount(new Account("Savings",0.0))) {
 					return "New savings account created.\n" + showMyAccounts(customer);
